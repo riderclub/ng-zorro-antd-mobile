9b414914295879337df93ce5c81cafcdf1167516
Vanessa Pasque (Lith) <lith@amykron.com>
2021-04-02 22:36:36 +0200

    Add missing error translate

--- components/date-picker/locale/en_US.ts
+++ components/date-picker/locale/en_US.ts
@@ -1,6 +1,9 @@
 // import  DatePickerLocale from 'rmc-date-picker/lib/locale/en_US';
 
 export default {
+  errorMessage: 'The current time is inconsistent with the set maximum or minimum date format',
+  curTMorethanMax: 'The current time is greater than the set maximum date',
+  curTLessthanMin: 'The current time is less than the set minimum date',
   okText: 'OK',
   dismissText: 'Cancel',
   extra: 'please select',
--- components/date-picker/locale/fr_FR.ts
+++ components/date-picker/locale/fr_FR.ts
@@ -1,6 +1,9 @@
 // import  DatePickerLocale from 'rmc-date-picker/lib/locale/en_US';
 
 export default {
+  errorMessage: 'La date sélectionné est incompatible avec les autres dates',
+  curTMorethanMax: 'La date sélectionné est supérieure à la date maximale définie',
+  curTLessthanMin: 'La date sélectionné est inférieur à la date minimale définie',
   okText: 'OK',
   dismissText: 'Annuler',
   extra: 'sélectionner',
